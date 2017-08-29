52e3bb83d60c833a04dbffca48a5973df8f96a8d
Michal Moskal <michal@moskal.me>
2017-08-29 11:55:16 +0100

    Fix export syntax

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 export {CortexM} from "./cortex/cortex";
 export {CortexReg, CortexSpecialReg, CoreState, CoreNames, ISANames} from "./cortex/constants";
-export * from "./dap/dap";
+import DAP from "./dap/dap";
+export {DAP};
 
 export {FlashTargets, FlashTarget} from "./targets/FlashTarget";
 export {FlashProgram} from "./targets/FlashProgram";
